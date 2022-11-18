d5736c5eedae1a25aeba7a5a71348e69d6f04345
KimChily1117 <57296811+KimChily1117@users.noreply.github.com>
2022-11-18 16:43:41 +0900

    12312321
    1231231231

---
+++ Runtime/XLua/Editor/LuaScriptAssetEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+[InitializeOnLoad]
+[CustomEditor(typeof(LuaScriptAsset))]
+public class LuaScriptAssetEditor : Editor
+{
+    LuaScriptAsset thisAsset;
+    public override void OnInspectorGUI()
+    {
+        thisAsset = target as LuaScriptAsset;
+        EditorGUILayout.TextArea(thisAsset.src);
+    }
+}
